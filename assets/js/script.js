bec76dbfa8447c0b5b34c9b40f5c701ae642180a
PratimaGurav <prtmgrv2@gmail.com>
2021-09-23 16:52:12 +0000

    Updating js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,58 @@ const scoreCount = document.getElementById("score");
 const answers = Array.from(document.getElementsByClassName("answer-text"));
 const progressCount = document.getElementById("question-numbers");
 const progressFullBar = document.getElementById("complete-progress");
+const correctScore = 20;
+const maxQuestions = 5;
 
 let questionCounter = 0;
 let score = 0;
 let currentQuestion = {};
-let availableQuestions = questions;
+let availableQuestions = [];
 let acceptAnswers = true;
-const correctScore = 20;
-const maxQuestions = 5;
+
+//Referred James Q Quick video on YouTube 
+
+// Fetch questions from Json file
+
+fetch('./assets/js/questions.json')
+    .then(res => res.json())
+    .then(data => {
+        questions = data;
+        startGame()
+    })
+    .catch((error) => {
+        alert("Sorry unable to load questions.")
+})
+
+// Start game function
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
 
 if(questionCounter >= maxQuestions) {
     localStorage.setItem('currentRoundScore', score);
     return window.location.assign("./game-over.html");
 }
+//Updates Progress Bar
 questionCounter ++;
 progressCount.innerText = `Question ${questionCounter} / ${maxQuestions}`;
 progressFullBar.style.width = `${(questionCounter/maxQuestions)* 100}%`;
+
+//Updates question and choices 
 const questionSelecter = Math.floor(Math.random() * availableQuestions.length);
 currentQuestion = availableQuestions[questionSelecter];
 questionText.innerText = currentQuestion.questions;
-answers.forEach(answer => {
-    const number = answer.dataset.number;
-    answer.innerText = currentQuestion[number];
+
+// Setting choices
+choices.forEach(choice => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice'+ number];
 });
 
+// Removing used questions
 availableQuestions.splice(questionSelecter, 1);
 acceptAnswers = true;
 
@@ -35,14 +63,16 @@ function increaseScore(total){
     scoreCount.innerText = score; 
 }
 
-answers.forEach(answer => {
-    answers.addEventListener("click", function sortAnswer(e){
+// Attaching click event to all answers
+choices.forEach(choice => {
+    choice.addEventListener("click", function sortAnswer(e){
         if(!acceptAnswers) return; // if answer not accepted end the function.
         acceptAnswers = false; // set accept answers to false once an answer has been selected.
         const selectedOption = e.target;
-        const selectedAnswer = selectedOption.dataset.number;
+        const selectedAnswer = selectedOption.dataset[number];
 
-        let classToApply = selectedAnswer === currentQuestion.correctAnswer ? "correct-answer" : "wrong-answer";
+        //Styling correct or incorrect answers choosen
+        let classToApply = selectedAnswer == currentQuestion.correctAnswer ? "correct-answer" : "wrong-answer";
 
         if(classToApply === "correctanswer") {
             increaseScore(correctScore);
@@ -55,13 +85,7 @@ answers.forEach(answer => {
     });
 });
 
-//To start quiz game, set start value to 0, run getNewQuestion function.
-function startQuiz() {
-    questionCounter = 0;
-    score = 0;
-    getNewQuestions();
-    console.log("Good Luck!");
-}
 
-startQuiz();
+
+
 
