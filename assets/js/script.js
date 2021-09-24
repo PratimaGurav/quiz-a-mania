6c850dc7292cd41c247ea96f15eb5db0345aadc8
PratimaGurav <prtmgrv2@gmail.com>
2021-09-24 19:59:45 +0000

    Update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,10 +71,18 @@ choices.forEach((choice) => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
         const classToApply =
-        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-  
-      selectedChoice.parentElement.classList.add(classToApply);
-  
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"; 
+
+        if (classToApply === "correct") {
+            incrementScore(correctScore);
+          }
+      
+          selectedChoice.parentElement.classList.add(classToApply);
+      
+          setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+          }, 1000);
     });
 });
 
