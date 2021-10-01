068f03272f396668482629502cb6cfb9b62f7ba9
PratimaGurav <prtmgrv2@gmail.com>
2021-10-01 17:34:40 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,7 @@ choices.forEach((choice) => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
-          }, 1000);
+          }, 1300);
     });
 });
 
