90b8d0af0da4cab7be4d9f0f32c4454668e894f6
PratimaGurav <prtmgrv2@gmail.com>
2021-10-08 15:47:35 +0000

    Add and modify comments and codes in game-over.js

--- assets/js/game-over.js
+++ assets/js/game-over.js
@@ -1,3 +1,4 @@
+/*jshint esversion:6*/
 const username = document.getElementById('username');
 const saveScore = document.getElementById('saveScore');
 const finalScore = document.getElementById('finalScore');
@@ -6,13 +7,14 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const Max_HighScores = 10;
 
+// Set final score to Most Recent Score.
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
+function saveHighScore(e){
     e.preventDefault();
 
     const score = {
