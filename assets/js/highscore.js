906df795e0c8a4e645f544c863dd144b09ae44ee
PratimaGurav <prtmgrv2@gmail.com>
2021-10-08 15:52:28 +0000

    Add and modify comments and codes in highscore.js

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,13 +1,13 @@
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-highScoresList.innerHTML =  highScores
-    .map(score => {
+highScoresList.innerHTML =  highScores.map(
+    function (score){
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
 })
 .join('');
 
-// Clear highScore
+// Clear highScore from local storage, alert the user to confirm the action when clicked. An onclick event called in highscore.html.
 function clearScores() {
     if (confirm("Do you want to clear the high scores ?") == true) {
         localStorage.clear();
